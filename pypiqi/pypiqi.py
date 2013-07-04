5e747d6aececa42dd31b293a61bcf182a18f1bba
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2013-07-04 14:48:47 +0200

    WIP working on client implementation

--- pypiqi/pypiqi.py
+++ pypiqi/pypiqi.py
@@ -3,39 +3,52 @@ import json as json_mod
 import subprocess
 import struct
 
-from gen.piqi_tools.piqi_pb2 import convert_input, json, pb, xml, piq
-#from gen.piqi.piqi_pb2 import
+from gen.piqi_tools.piqi_pb2 import convert_input, add_piqi_input, \
+        json, pb, xml, piq
+from gen.piqi_rpc.piqi_pb2 import request as piqi_rpc_request, \
+        response as piqi_rpc_response
+
 
 class piqi(object):
 
     def __init__(self):
         self._server = subprocess.Popen(["piqi", "server"],
                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        self._add_piqi = []
+        self._added_piqi = set()
 
     def __del__(self):
         self._server.kill()
 
     @staticmethod
     def _encode_rpc_request(name, bininput):
+        req = request()
+        req.name = name
+        req.data = bininput
+        return req
 
-
-    def _add_to_piqi(self, obj):
+    def _add_piqi(self, obj):
         """Send protobuf-encoded piqi specification to server"""
-        self._send(obj._piqi_defn_pb)
+        inp = add_piqi_input()
+        inp.format = pb
+        inp.data = obj._piqi_defn_pb
+        self._rpc(inp.SerializeToString())
 
-    def _send_and_receive(self, packet):
-        length = struct.pack(">I", len(packet))
-        writable = length + packet
-        print(''.join(["\\x%02X" % ord(x) for x in writable]))
-        self._server.stdin.write(length + packet)
+    def _rpc(self, request_name, payload):
+        if self._pdict.get(obj.DESCRIPTOR.name) != "add_piqi":
+            self._add_piqi(obj)
+            self._pdict[obj.DESCRIPTOR.name] = "add_piqi"
+        req = piqi_rpc_request()
+        req.name = obj.DESCRIPTOR.name
+        req.data = payload
+        command = "\x00\x00\x00\x00" + req.SerializeToString()
+        self._server.stdin.write(command)
         self._server.stdin.flush()
-
         len_back = self._server.stdout.read(4)
-        print(''.join(["\\x%02X" % ord(x) for x in len_back]))
         len2 = struct.unpack(">I", len_back)
+        resp = self._server.stdout.read(len2)
+        ret = piqi_rpc_response()
+        return ret.ParseFromString()
 
-        return self._server.stdout.read(len2)
 
 
     def generate(self, obj, output_fmt_str, json_omit_null_fields=True,
